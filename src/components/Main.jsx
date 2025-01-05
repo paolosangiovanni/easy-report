054b69ac3453143d30e49f116dee29c1c0d2b3d2
paolosangiovanni <82282354+paolosangiovanni@users.noreply.github.com>
2025-01-05 12:30:24 +0100

    first commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import './main.css'
 
-import PageTitle from './PageTitle'
-
 function Main() {
   return (
     <main id='main' className='main'>
-        <PageTitle id='breadcrumb-shift' pagename={'Report Overview'}page={'/'}/>
+
     </main>
   )
 }
